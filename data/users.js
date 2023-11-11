d3b0c8b329fcaedc545fd55627ec7405b961b3d7
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-11 14:56:22 +0300

    Defined users' module

---
+++ data/users.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("./db");
+
+const users = sequelize.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
+
+module.exports = { users };
