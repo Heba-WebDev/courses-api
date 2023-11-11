db96943ee94bfc3927ca582e631151124e1439ef
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-11 14:55:53 +0300

    Created controllers for users' routes

---
+++ controllers/users.controller.js
@@ -0,0 +1,117 @@
+require("dotenv").config();
+const { users } = require("../data/users");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const generateJwt = require("../utils/generateJWT");
+const asyncWrapper = require("../middlewares/asyncWrapper");
+const { SUCCESS, FAIL } = require("../utils/httpStatusCode");
+const appError = require("../utils/globalError");
+
+const getAllUsers = asyncWrapper(async (req, res, next) => {
+  const page = req.query.page ? parseInt(req.query.page) : 1;
+  const limit = req.query.limit ? parseInt(req.query.limit) : 2;
+  const offset = (page - 1) * limit;
+  const totalUsers = await users.count();
+  const totalPages = Math.ceil(totalUsers / limit);
+
+  const result = await users.findAll({
+    attributes: { exclude: ["password"] },
+    limit: limit,
+    offset: offset,
+  });
+
+  if (result?.length === 0) {
+    const err = appError.create("No user found!", 404, FAIL);
+    return next(err);
+  }
+  res.send({
+    status: SUCCESS,
+    data: result,
+    totalPages: totalPages,
+  });
+});
+
+const getUser = asyncWrapper(async (req, res, next) => {
+  const user = await users.findOne({
+    where: { id: req.params.id },
+  });
+
+  if (!user) {
+    const err = appError.create("User not found.", 404, FAIL);
+    return next(err);
+  }
+
+  return res.status(200).json({
+    status: SUCCESS,
+    data: course,
+  });
+});
+
+const signup = asyncWrapper(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = await users.findOne({
+    where: { email: req.body.email },
+  });
+
+  if (user) {
+    const err = appError.create("Email exists", 400, FAIL);
+    return next(err);
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const token = await generateJwt({ name: name });
+  users
+    .create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    })
+    .then((result) => {
+      return res.status(201).json({
+        status: SUCCESS,
+        data: {
+          data: { result, accessToken: token },
+          message: "User sucessfully created.",
+        },
+      });
+    });
+});
+
+const login = asyncWrapper(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    const err = appError.create("Email and password are required.", 400, FAIL);
+    return next(err);
+  }
+  const user = await users.findOne({
+    where: { email: email },
+  });
+
+  if (!user) {
+    const err = appError.create("User not found.", 400, FAIL);
+    return next(err);
+  }
+
+  const matchedPassword = await bcrypt.compare(password, user.password);
+  const token = await generateJwt({ name: user.name });
+
+  if (!matchedPassword) {
+    const err = appError.create("Invalid Credentials.", 401, FAIL);
+    return next(err);
+  } else {
+    return res.status(200).json({
+      status: SUCCESS,
+      data: {
+        data: { name: user.name, email: user.email, accessToken: token },
+        message: "User sucessfully loged in.",
+      },
+    });
+  }
+});
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  signup,
+  login,
+};
